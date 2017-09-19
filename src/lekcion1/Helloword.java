9c1ce3e7c19c3d15a278b1d27b914321864eb59c
alk1983 <oleg_karpatschew@tut.by>
2017-09-19 20:19:57 +0300

    lekcion 1

---
+++ src/lekcion1/Helloword.java
@@ -0,0 +1,11 @@
+package lekcion1;
+
+public class Helloword {
+    public static void main(String[] args ){
+        System.out.println ("hello word");
+    }
+public Robot (int a_x,int a_y){
+
+
+}
+}
