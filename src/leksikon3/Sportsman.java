bc0d64307c31c6fcae5732ec8f2c68d96cfb0919
alk1983 <oleg_karpatschew@tut.by>
2017-09-28 11:54:47 +0300

    lekcion4

---
+++ src/leksikon3/Sportsman.java
@@ -0,0 +1,14 @@
+package leksikon3;
+
+public class Sportsman {
+    public class  Sportsman1 {
+        String name;
+         int zoloto;
+         int serebro;
+    }
+    public static void main(String[] args ){
+        Sportsman1 sport = new Sportsman1();
+
+
+    }
+}
